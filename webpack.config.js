fbf85e43a959df963b64f48696cb813e2573e360
BoE <857143901@qq.com>
2018-08-06 22:08:56 +0800

    配置环境成功

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
     entry: './index.js',
     output: {
-        path: './public',
-        filename: 'app.js'
-    }
+        path: __dirname + '/public',
+        filename: 'build.js'
+    },
+    devtool: 'eval-map-source',
+    devServer: {
+        contentBase: path.join(__dirname,'public'),
+        port: 8000,
+        historyApiFallback: true,
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$|\.jsx$/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['es2015','react','stage-0']
+                    }
+                }
+            }     
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
 }
